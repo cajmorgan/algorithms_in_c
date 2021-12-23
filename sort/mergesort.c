e6575ab4d4af5b8d321deb98dbff548699e0707d
cajmorgan <caj@cajmorgan.com>
2021-12-23 17:28:38 +0100

    finalizing mergesort

--- sort/mergesort.c
+++ sort/mergesort.c
@@ -19,10 +19,42 @@ void merge_sort(int A[], int length) {
  * Best case performance: Ω(n)
  * General performance: O(log2(n/2))
  * Worse case performance: O(log2n)
- * Ex. [0, 2] [1, 4]
- * left_arrow = start, right_arrow = middle
- * check smallest, push to temp, increase that one.
+ * Ex. Arr1[0, 2] Arr2[1, 4]
+ * left_arrow = start, right_arrow = middle(start of Arr2)
+ * check smallest, push to temp, move that arrow one step to right.
+ * Example:
+ * TempArr = [0, 2, 1, 4]
+ * Arr1 = [0, 2] 
+ * Arr2 = [1, 4]
+ * ***********
+ * 1. 
+ * [*0*, 2] [*1*, 4]
+ * [*0*, 2, 1, 4] 
+ * 2.
+ * [0, *2*] [*1*, 4]
+ * [0, *1*, 1, 4] 
+ * 3.
+ * [0, *2*] [1, *4*]
+ * [0, 1, *2*, 4] 
+ * 4.
+ * [0, 2*]* [1, *4*]
+ * [0, 1, 2, *4*] 
  * 
+ * Now copy temp_arr to original arr sent in.
+ * 
+ * Without optimization the performance will always be O(log2n * n)
+ * With optimization we can get Ω(n) in certain scenarios.
+ * Optimization implemented: 
+ * As both subarrays will guaranteed be sorted by themselves, 
+ * we can check for the last value in the first array.
+ * [0, *2*]
+ * if that value is smaller than [*1*, 4] (which it isn't in this case)
+ * we can break just merge the arrays, as they will already be sorted between each other.
+ * 
+ * A second optimization we can do is checking if the current right_arrow value 
+ * is larger than last value of Arr1(middle-1) && left_arrow has incremented to middle. 
+ * In that case, we know that the following right_arrow values will be higher, and therefore 
+ * already are in the correct places in the A array. Break the loop. 
  *  **/
 void sort_splits(int A[], int temp_arr[], int start, int middle, int end, int org_length) {
   int left_arrow = start;
@@ -30,12 +62,12 @@ void sort_splits(int A[], int temp_arr[], int start, int middle, int end, int or
   int picker = start;
   
   while(right_arrow < end || left_arrow < middle) {
-    if (A[middle] > A[middle-1]) 
+    if (A[middle-1] < A[middle]) 
       break;
     
     if (A[middle-1] < A[right_arrow] && left_arrow >= middle)
       break;
-
+    
     counter++;
     if ((A[left_arrow] <= A[right_arrow] || right_arrow >= end) && left_arrow < middle) {
       temp_arr[picker++] = A[left_arrow];
@@ -63,14 +95,24 @@ void split_merge(int A[], int temp_arr[], int start, int end, int org_length) {
 
 
 int main() {
-  int arr_to_sort[] = {16, 21, 32, 14, 16, 19, 20, 32, 12, 16 , 32, 12};
+  int arr_to_sort[] = {3,1,2,7,1,2,4,2,3,78,4,5};
   int length = 12;
   merge_sort(arr_to_sort, length);
 
-  int arr_test[12];
-  for (int i = 0; i < 12; i++) {
+  int arr_test[length];
+  for (int i = 0; i < length; i++) {
     printf("%d\n", arr_to_sort[i]);
+    // if (i == 0) {
+    //   printf("{%d,", abs((i * rand()) % 100));
+    //   continue;
+    // } else if (i == 100000-1) {
+    //   printf("%d}", abs((i * rand()) % 100));
+    //   continue;
+    // }
+
+    // printf("%d,", abs((i * rand()) % 100));
   }
+
 
   printf("ITERATIONS: %d\n", counter);
 
